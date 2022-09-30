58670c1c044cf7f6e8b9fd6ee195aad7065ac4ba
Marcin Jerzak <hey@jerzakm.com>
2022-10-01 01:19:51 +0200

    - sveltekit - tailwind - font menu stuff

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '../app.postcss';
+	import '../styles/global.css';
+</script>
+
+<main class="flex flex-col items-center h-screen justify-center">
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+</script>
+
+<container class="flex flex-col gap-8 items-center">
+	<h1 class="text-5xl">Game title</h1>
+	<p class="game-desc">
+		The world is full of secrets. You're an impatient wizard who crawls dungeons by duplicating
+		himself. Although you can be in many places at the same time, your powers and life are not
+		multiplicated. Be careful or you may overstretch yourself!
+	</p>
+
+	<menu class="flex flex-col gap-2">
+		<a
+			href="/"
+			class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center"
+		>
+			Explore the world
+		</a>
+		<a
+			href="/"
+			class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center"
+		>
+			Options
+		</a>
+	</menu>
+</container>
+
+<style>
+	.game-desc {
+		width: calc(6rem + 30vmax);
+	}
+</style>
